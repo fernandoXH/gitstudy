de63dfd9cae369ce33f6933f1ba9460199cd5c77
root <root@xionghui.dev>
2015-07-30 15:38:47 +0800

    v2.0

--- index.php
+++ index.php
@@ -1,12 +1,2 @@
 <?php
-<<<<<<< HEAD
-    echo 'ceshi tag 1.0 roll back';
-    echo 'mast update index bug';
-    echo 'xh-dev write';
-    echo '133';
-=======
-    echo 'master的tag测试';
-    echo 'ceshi tage v1.2';
->>>>>>> 130e45528bed3c3d2dc7d7b11be85af47978b298
-    echo 'dev继续开发新功能 gogogo';
-    echo 'test tag';
+echo 'v2.0 start';
