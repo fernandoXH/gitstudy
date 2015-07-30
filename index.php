b8ab61f73fc6b645995e9961e9e531edfdf2268c
root <root@xionghui.dev>
2015-07-30 14:35:00 +0800

    修改

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-    echo 'ceshi tag 1.0';
     echo 'mast update index bug';
-    echo 'xh-dev write';
-    echo '133';
+    echo 'jjsdhfksjdfksdj';
     echo 'dev继续开发新功能 gogogo';
     echo 'test tag';
